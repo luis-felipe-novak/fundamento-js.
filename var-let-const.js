49c2a864d084aa7f12feb43a993eaad58ef100a8
luis-felipe-novak <luis.novak@escola.pr.gov.br>
2026-03-10 14:15:41 +0000

    variaveis

---
+++ var-let-const.js
@@ -0,0 +1,19 @@
+// var
+
+var altura = 5;
+var comprimento = 7;
+
+area = altura * comprimento;
+console.log(area);
+var area;
+
+let forma = 'retangulo';
+let altura = 5;
+let comprimento =7
+let area;
+
+//Se(forma igual 'retangulo')faça
+if (forma === ' retangulo'){
+    area = altura * comprimento;
+}else {
+    area = (altura * compriment)/ 2;
