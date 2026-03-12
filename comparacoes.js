f920b2bb8179ed00ac8dc2c58dbf2c7661d5f760
luis-felipe-novak <luis.novak@escola.pr.gov.br>
2026-03-12 12:02:02 -0300

    comparacao

---
+++ comparacoes.js
@@ -0,0 +1,13 @@
+// == (comparação implícita)
+
+const numero = 5;
+const texto ="5";
+
+console.log(numero === texto)
+
+// typerof
+console.log(typeof numero);
+console.log(typeof texto);
+
+Number()
+String()
